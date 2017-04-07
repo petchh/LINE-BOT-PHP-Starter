7052ae0ed74c9b880825c462ca3c778e0a784d48
petchh <keeratiphet@gmail.com>
2017-04-07 17:20:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,7 +22,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $ans
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
