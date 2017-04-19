03fc812accb7d4b9e4a9b3119044a3acd99c0b50
petchh <keeratiphet@gmail.com>
2017-04-19 22:03:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,9 +1,6 @@
 <?php
 $access_token = 'mdUfi036U4HKokM8zkcdBmj5imHO0bw7lvrCHU6oMuA4y2U+dM0kjKqgpSwYVz6r/5Dn3S7qL+OyYaI2S7YTivOsVtD3Oy9S7yivMUAnvPZxHdPPT6Vkl+LjO3PncXf913PbKdos1qkWz1oOPOCQswdB04t89/1O/w1cDnyilFU=';
-$msg="";
-$m_type="";
-$regs="";
-$msg_check="";
+
 
 
 // Get POST body content
@@ -24,47 +21,6 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			
-			if((eregi ( "วิธีลงทะเบียน", $text, $regs ))or(eregi ( "การลงทะเบียน", $text, $regs ))){
-				$messages = [
-					'type'=>'text',
-					'text' =>'ทำตามขั้นตอนตามนี้เลยครับ http://reg.mju.ac.th/enrollguide.htm'
-				];
-			}else if((eregi ( "Transcript", $text, $regs ))or(eregi ( "ทรานสคริป", $text, $regs ))){
-				$messages = [
-					'type'=>'text',
-					'text' =>'เข้า www.education.mju.ac.th แล้วเลือก เข้าสุ่ระบบนักศึกษาครับ'
-				];
-			}else if((eregi ( "คศ101", $text, $regs ))or(eregi ( "แคลคูลัส 1", $text, $regs ))){
-				$messages = [
-					'type'=>'text',
-					'text' =>'คศ101	แคลคูลัส 1
-	Course Description:
-	ลิมิตและความต่อเนื่อง อนุพันธ์ การประยุกต์ของอนุพันธ์ ดิฟเฟอเรนเชียลและอินทริกัลป์ไม่จำกัดเขต อินทริกัลป์จำกัดเขต และการประยุกต์ อนุพันธ์ย่อย
-	Limit and continuity of functions, the derivative of algebraic functions and transcendental functions, the indefinite integrals and techniques of integration, and the definite integrals with applications
-'
-				];
-			}else if((eregi ( "ลงทะเบียนต่ำกว่า 9 หน่วย", $text, $regs ))or(eregi ( "ลงทะเบียนน้อยกว่า 9 หน่วย", $text, $regs ))){
-				$messages = [
-					'type'=>'text',
-					'text' =>'แนะนำให้ดาวน์โหลด มจท32 ที่เว็บ education.mju.ac.th แล้วยื่นที่งานตารางสอนครับ'
-				];
-			}else if((eregi ( "ขาดความอบอุ่น", $text, $regs ))or(eregi ( "ต้องการคนสนใจ", $text, $regs ))or(eregi ( "อยากให้มีคนสนใจ", $text, $regs ))){
-				$messages = [
-					'type'=>'text',
-					'text' =>'ถ้าอยากให้ใครดีกับเราเราต้องทำดีกับเขาก่อนดูนะครับ'
-				];
-			}else if((eregi ( "ทะเลาะกับแฟน", $text, $regs ))or(eregi ( "ผิดกับแฟน", $text, $regs ))){
-				$messages = [
-					'type'=>'text',
-					'text' =>'การทะเลาะกันไม่ทำให้ใครมีความสุขหรอกนะครับ ลองใจเย็นๆแล้วคุยกันดีๆนะ ^^'
-				];
-			}else if((eregi ( "ชั่วโมงกิจกรรม", $text, $regs ))or(eregi ( "เวลาโมงกิจกรรม", $text, $regs ))){
-				$messages = [
-					'type'=>'text',
-					'text' =>'สามารถตรวจสอบจำนวนชั่วโมงกิจกรรมได้ที่ www.erp.mju.ac.th/student/signin.aspx?/typeAuthentication=1'
-				];
-				
-			}
 
 			// Build message to reply back
 			$messages = [
