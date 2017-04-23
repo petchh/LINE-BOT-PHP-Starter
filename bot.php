6940f437e71c6f96784bbaf73b2e34f619072202
petchh <keeratiphet@gmail.com>
2017-04-23 17:47:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -76,8 +76,7 @@ if (!is_null($events['events'])) {
 				$messages = [
 					'type'=>'text',
 					'text' =>'บายนะ! เวลาเหงาทักหา Bot นะ'
-				];
-				
+				];		
 			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
