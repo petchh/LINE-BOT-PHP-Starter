0253de5f0258789f87e07ba9e2b23ef3eec988a7
petchh <keeratiphet@gmail.com>
2017-05-08 11:47:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -149,7 +149,18 @@ if (!is_null($events['events'])) {
 					'type'=>'text',
 					'text' =>'บายนะ! เวลาปรึกษาทักหา Bot นะ :)'
 				];		
-			}
+			}else if((eregi ( "เปิดประตู", $text ))or(eregi ( "open", $text ))){
+				$messages = [
+					'type'=>'text',
+					'text' =>'Open, Ok'
+				];
+			
+			}else if((eregi ( "ปิดประตู", $text ))or(eregi ( "close", $text ))){
+				$messages = [
+					'type'=>'text',
+					'text' =>'Close, Ok'
+				];
+			}	
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
